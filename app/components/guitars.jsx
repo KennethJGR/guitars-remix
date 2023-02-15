45f8489d43100be4ff4a4681917366bd9fb1c0e6
Kenneth Gaitan <gaitan.kj@gmail.com>
2023-02-14 19:25:16 -0600

    finishing css store

---
+++ app/components/guitars.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "@remix-run/react";
+
+const Guitars = ({ guitar }) => {
+    const { name, price, description, image, url } = guitar;
+
+    console.log();
+    return (
+        <div className="guitars">
+            <img
+                src={image.data.attributes.formats.medium.url}
+                alt={`guitar ${name}`}
+            />
+            <div className="content">
+                <h3>{name}</h3>
+
+                <p className="description">{description}</p>
+                <p className="price">${price}</p>
+
+                <Link to={`/guitars/${url}`}>
+                    <button className="btn">View</button>
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default Guitars;
--- app/routes/store.jsx
+++ app/routes/store.jsx
@@ -1,14 +1,35 @@
 import React from "react";
+import { useLoaderData } from "@remix-run/react";
 import { getGuitars } from "../../app/models/guitars.server.js";
+import Guitars from "~/components/guitars.jsx";
+
+import styles from "../styles/guitars.css";
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
+}
 
 export async function loader() {
-  getGuitars();
-
-  return import("./store");
+  const guitars = await getGuitars();
+  return guitars.data;
 }
 
 const Store = () => {
-  return <div>Store</div>;
+  const guitars = useLoaderData();
+
+  return (
+    <main className="container">
+      <h2 className="heading">Our Collection</h2>
+
+      {guitars?.length && (
+        <div className="guitars-grid">
+          {guitars.map((guitar) => (
+            <Guitars key={guitar?.id} guitar={guitar?.attributes} />
+          ))}
+        </div>
+      )}
+    </main>
+  );
 };
 
 export default Store;
